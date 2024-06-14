28793c242f706b869e55578b2775db601533707b
David Borrego <davidbf798@gmail.com>
2024-06-14 13:35:05 +0200

    Initial Commit

---
+++ src/test/java/com/uniquedreamtravel/udt/UdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquedreamtravel.udt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
